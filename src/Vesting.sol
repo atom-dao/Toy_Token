b868b0376e1074f1c282e54f291ec9417a62c600
Pelumi527 <fatolupelumi@gmail.com>
2022-06-24 12:05:25 +0100

    fix readme

--- src/Vesting.sol
+++ src/Vesting.sol
@@ -4,22 +4,4 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract VestingContract {
 
-    ERC20 toytoken;
-
-    address public tokenContract;
-    
-    function setTokenContract(address _tokenContract) public returns(bool){
-        tokenContract = _tokenContract;
-        return true;
-    }
-
-    function balance() public returns(uint256){
-        toytoken = ERC20(tokenContract);
-        return toytoken.balanceOf(address(this));
-    }
-
-    function transfer(address account, uint256 amount) public returns(bool){
-        toytoken.transfer(account,amount );
-        return true;
-    }
 }
